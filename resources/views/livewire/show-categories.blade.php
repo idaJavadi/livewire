7529a19c3c8e1e9a936800a9a1058c8e87648a95
ida <idajavadii11@gmail.com>
2025-07-31 10:38:00 +0630

    livewire install

---
+++ resources/views/livewire/show-categories.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>this is show categories</h1>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @livewireStyles
+</head>
+
+
+<body>
+<h1>
+    this is test
+    @livewire('show-categories')
+    <livewire:category.show/>
+</h1>
+@livewireScripts
+</body>
+</html>
